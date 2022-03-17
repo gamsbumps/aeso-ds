25d648633161f4da0ab15899eb7b8a00e561a905
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-17 20:39:58 -0300

    Printing area

--- src/NumberTwo/RectangleApplication.java
+++ src/NumberTwo/RectangleApplication.java
@@ -3,5 +3,6 @@ package NumberTwo;
 public class RectangleApplication {
     public static void main(String[] args) {
         Rectangle rec = new Rectangle(15, 18);
+        System.out.println("Area" + rec.area() + "m²");
     }
 }
