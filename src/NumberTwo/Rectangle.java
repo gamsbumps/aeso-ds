5578147f1dbcb3f228e677e4cd63bff1866d8fe1
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-17 20:27:47 -0300

    Creating getter and setter

--- src/NumberTwo/Rectangle.java
+++ src/NumberTwo/Rectangle.java
@@ -3,6 +3,14 @@ package NumberTwo;
 public class Rectangle {
     int side = 0;
 
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
     Rectangle(){
     }
 
