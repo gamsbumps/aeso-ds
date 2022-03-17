34170baca94984ac0b3a9bc436f7b41bb29c6e19
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-17 20:48:11 -0300

    variable and getter and setter

---
+++ src/NumberThree/HorseShoe.java
@@ -0,0 +1,13 @@
+package NumberThree;
+
+public class HorseShoe {
+    int horseshoe = 0;
+
+    public int getHorseshoe() {
+        return horseshoe;
+    }
+
+    public void setHorseshoe(int horseshoe) {
+        this.horseshoe = horseshoe;
+    }
+}
