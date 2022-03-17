92bccb9d66d567847e0c8c8e8427f841fd034bdb
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-17 20:51:13 -0300

    creating constructors

--- src/NumberThree/HorseShoe.java
+++ src/NumberThree/HorseShoe.java
@@ -7,7 +7,19 @@ public class HorseShoe {
         return horses;
     }
 
-    public void setHorseshoe(int horseshoe) {
-        this.horses = horseshoe;
+    public void setHorses(int horses) {
+        if (horses > 0){
+            this.horses = horses;
+        }
+        else{
+            System.out.println("Number of horses cannot be lower than 1!");
+        }
+    }
+
+    public HorseShoe() {
+    }
+
+    public HorseShoe(int horses) {
+        this.setHorses(horses);
     }
 }
