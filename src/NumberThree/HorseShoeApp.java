da7dca678cf61d34a7ebddd41f7e3ae5d69841e3
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-17 21:11:55 -0300

    creating scanner and variable horse

---
+++ src/NumberThree/HorseShoeApp.java
@@ -0,0 +1,12 @@
+package NumberThree;
+
+import java.util.Scanner;
+
+public class HorseShoeApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int horse;
+
+    }
+}
