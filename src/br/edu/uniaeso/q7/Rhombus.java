327b8b7320e4d0bc4dcb92b030f6dc6f9831dc0e
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-26 12:17:28 -0300

    getters and setters

---
+++ src/br/edu/uniaeso/q7/Rhombus.java
@@ -0,0 +1,21 @@
+package br.edu.uniaeso.q7;
+
+public class Rhombus {
+    private int diagonal1, diagonal2;
+
+    public int getDiagonal1() {
+        return diagonal1;
+    }
+
+    public void setDiagonal1(int diagonal1) {
+        this.diagonal1 = diagonal1;
+    }
+
+    public int getDiagonal2() {
+        return diagonal2;
+    }
+
+    public void setDiagonal2(int diagonal2) {
+        this.diagonal2 = diagonal2;
+    }
+}
