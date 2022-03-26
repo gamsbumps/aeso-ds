3e55b7491f622c48572af22744e9fadf5954317c
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-26 11:29:52 -0300

    Creating getters and setters

---
+++ src/br/edu/uniaeso/q5/Triangle.java
@@ -0,0 +1,28 @@
+package br.edu.uniaeso.q5;
+
+public class Triangle {
+    private int a, b, h;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+}
