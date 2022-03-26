1607ea44f79299bb0fb1ee0163fae009b3159d65
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-26 12:43:28 -0300

    constructors

--- src/br/edu/uniaeso/q9/Triangles.java
+++ src/br/edu/uniaeso/q9/Triangles.java
@@ -26,4 +26,16 @@ public class Triangles {
     public void setC(int c) {
         this.c = c;
     }
+
+    public Triangles(int a, int b, int c) {
+        setA(a);
+        setB(b);
+        setC(c);
+    }
+
+    public Triangles() {
+        setA(0);
+        setB(0);
+        setC(0);
+    }
 }
