aedc35740c990481ff2bd77b99546281787c3167
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-26 14:39:33 -0300

    method play rock paper scissors

--- src/br/edu/uniaeso/q10/RockPaperScissors.java
+++ src/br/edu/uniaeso/q10/RockPaperScissors.java
@@ -1,27 +1,67 @@
 package br.edu.uniaeso.q10;
 
 public class RockPaperScissors {
-    private int player;
+    private int computer;
+    private String player;
     public static int randInt(int min, int max){
         java.util.Random rand = new java.util.Random();
         int randomNum = rand.nextInt((max -min) + 1) + min;
         return randomNum;
     }
 
-    public int getPlayer() {
+    public String getPlayer() {
         return player;
     }
 
-    public void setPlayer(int player) {
+    public void setPlayer(String player) {
         this.player = player;
     }
 
-    public RockPaperScissors(int player) {
+    public int getComputer() {
+        return computer;
+    }
+
+    public void setComputer(int computer) {
+        this.computer = computer;
+    }
+
+    public RockPaperScissors(String player, int computer) {
         setPlayer(player);
+        setComputer(computer);
     }
 
     public RockPaperScissors() {
-        setPlayer(0);
+        setPlayer(" ");
+        setComputer(0);
+    }
+    public void playRPS(){
+        if(getPlayer().equals("ROCK")){
+            if(computer == 2){
+                System.out.println("Player win");
+            }else if (computer == 1){
+                System.out.println("Computer win");
+            }else{
+                System.out.println("Tie");
+            }
+        }
+        if(getPlayer().equals("PAPER")){
+            if (computer == 0){
+                System.out.println("Player win");
+            }else if (computer == 2){
+                System.out.println("Compter win");
+            }else{
+                System.out.println("Tie");
+            }
+        }
+        if(getPlayer().equals("SCISSORS")){
+            if(computer == 1){
+                System.out.println("Player win");
+            }else if(computer == 0){
+                System.out.println("Computer win");
+            }else{
+                System.out.println("Tie");
+            }
+        }
     }
 
 }
