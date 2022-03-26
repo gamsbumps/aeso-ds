756e0f9c031ae4941206e7741cba66c950c64835
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-26 12:00:09 -0300

    getter and setter

---
+++ src/br/edu/uniaeso/q6/MinimumWage.java
@@ -0,0 +1,13 @@
+package br.edu.uniaeso.q6;
+
+public class MinimumWage {
+    private float salary;
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
