626c176bbf6537f3ea5b110c22359be83b7084c7
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-26 11:18:23 -0300

    Getters and setters for GUI

---
+++ src/br/edu/uniaeso/q4/ConsoleGUI.java
@@ -0,0 +1,49 @@
+package br.edu.uniaeso.q4;
+
+import java.util.Scanner;
+
+/**
+ * Escreva um programa em java para ler o nome e a idade de uma pessoa,
+ * e exibir quantos dias de vida ela possui. Considere sempre anos completos,
+ * e que um ano possui 365 dias. Ex: uma pessoa com 19 anos possui 6935 dias de vida;
+ * veja um exemplo de saída: MARIA, VOCÊ JÁ VIVEU 6935 DIAS
+ */
+public class ConsoleGUI {
+    private String txtName;
+    private int txtAge;
+
+    private Scanner scanner;
+    private Person person;
+
+    public String getTxtName() {
+        return txtName;
+    }
+
+    public void setTxtName(String txtName) {
+        this.txtName = txtName;
+    }
+
+    public int getTxtAge() {
+        return txtAge;
+    }
+
+    public void setTxtAge(int txtAge) {
+        this.txtAge = txtAge;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
