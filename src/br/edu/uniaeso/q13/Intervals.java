c05d342cf3f0ea9b109ac1ba5d7f746967a2de4d
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-03-30 15:11:03 -0300

    Getter and setter

---
+++ src/br/edu/uniaeso/q13/Intervals.java
@@ -0,0 +1,47 @@
+package br.edu.uniaeso.q13;
+
+public class Intervals {
+    private int limitOne, limitTwo, sum, odds;
+
+    public int getLimitOne() {
+        return limitOne;
+    }
+
+    public void setLimitOne(int limitOne) {
+        if(limitOne < limitTwo){
+            this.limitOne = limitOne;
+        }else{
+            if(limitOne < (limitTwo -1)) {
+                System.out.println("There are no limits.");
+            }
+        }
+    }
+
+    public int getLimitTwo() {
+        return limitTwo;
+    }
+
+    public void setLimitTwo(int limitTwo) {
+        if(limitTwo > limitOne){
+            this.limitTwo = limitTwo;
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public int getOdds() {
+        return odds;
+    }
+
+    public void setOdds(int odds) {
+        this.odds = odds;
+    }
+
+
+}
