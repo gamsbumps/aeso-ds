7790b2c36cd448de036583c9e0d4aec27e170141
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-06-04 22:00:56 -0300

    Questão 14

--- src/br/edu/uniaeso/q14/ConsoleGUI.java
+++ src/br/edu/uniaeso/q14/ConsoleGUI.java
@@ -1,10 +1,9 @@
 package br.edu.uniaeso.q14;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ConsoleGUI {
-
+    private int guess = 1;
     private int txtPlayerOne, txtPlayerTwo;
     private int txtOption;
 
@@ -55,41 +54,56 @@ public class ConsoleGUI {
         setScanner(new Scanner(System.in));
     }
 
-    public void gui() {
-        int guess = 0;
-        System.out.println("Digite uma senha entre 0 e 100");
-        setTxtPlayerOne(getScanner().nextInt());
-        System.out.println((guess + 1) + "ª tentativa. Digite uma senha entre 0 e 100");
-        setTxtPlayerTwo(getScanner().nextInt());
-        setPw(new Password(getTxtPlayerOne(), getTxtPlayerTwo()));
-        if (getTxtPlayerOne() < 100 && getTxtPlayerTwo() > 0) {
-            while (true) {
-                if (getTxtPlayerTwo() == getTxtPlayerOne()) {
-                    System.out.println("Parabéns! Você acertou a senha.");
-                    break;
-                } else if (getTxtPlayerTwo() == getTxtPlayerOne() + 1 || getTxtPlayerTwo() == getTxtPlayerOne() - 1) {
-                    System.out.println("TÁ QUENTE");
-                    System.out.println((guess + 1) + "ª tentativa. Digite uma senha entre 0 e 100");
-                    setTxtPlayerOne(getScanner().nextInt());
-                    guess += 1;
-                } else {
-                    System.out.println("Você errou. Tente novamente");
-                    System.out.println((guess + 1) + "ª tentativa. Digite uma senha entre 0 e 100");
-                    setTxtPlayerOne(getScanner().nextInt());
-                    guess += 1;
-                }
-                if (guess == 5 && getTxtPlayerTwo() != getTxtPlayerOne()) {
-                    System.out.println("Você perdeu. Tente novamente depois.");
-                    System.out.println("Deseja continuar? 1-Sim 2-Não");
-                    setTxtOption(getScanner().nextInt());
-                    if(getTxtOption() == 1) {
-                        guess = 0;
-                    } else if(getTxtOption() == 2) {
-                        break;
-                    }
+  
 
+    public void gui() {
+        setPw(new Password(getTxtPlayerOne(), getTxtPlayerTwo()));
+        
+        while (true) {
+            if(guess == 1) {
+                System.out.println("Digite um número entre 0 e 100");
+                setTxtPlayerOne(getScanner().nextInt());
+                System.out.println("Jogador 2: Digite a senha entre 0 e 100");
+                setTxtPlayerTwo(getScanner().nextInt());
+            }
+            if (getTxtPlayerTwo() == getTxtPlayerOne()) {
+                System.out.println("Parabéns! Você acertou a senha.");
+                System.out.println("Deseja continuar? 1-Sim 2-Não");
+                setTxtOption(getScanner().nextInt());
+                if (getTxtOption() == 1) {
+                    guess = 1;
+                } else if (getTxtOption() == 2) {
+                    break;
+                }
+            } else if (getTxtPlayerTwo() == getTxtPlayerOne() + 1 || getTxtPlayerTwo() == getTxtPlayerOne() - 1) {
+                System.out.println("TÁ QUENTE");
+                System.out.println((guess + 1) + "ª tentativa. Digite uma senha entre 0 e 100");
+                guess += 1;
+                setTxtPlayerTwo(getScanner().nextInt());
+
+            } else {
+                System.out.println("Você errou. Tente novamente");
+                System.out.println((guess + 1) + "ª tentativa. Digite uma senha entre 0 e 100");
+                guess += 1;
+                setTxtPlayerTwo(getScanner().nextInt());
+            }
+            if (guess == 5 && getTxtPlayerTwo() == getTxtPlayerOne()) {
+                System.out.println("Voce ganhou.");
+                if (getTxtOption() == 1) {
+                    guess = 1;
+                } else if (getTxtOption() == 2) {
+                    break;
+                }
+
+            } else if (guess == 5 && getTxtPlayerTwo() != getTxtPlayerOne()) {
+                System.out.println("Você perdeu. Tente novamente depois.");
+                if (getTxtOption() == 1) {
+                    guess = 1;
+                } else if (getTxtOption() == 2) {
+                    break;
                 }
             }
+
         }
     }
 }
